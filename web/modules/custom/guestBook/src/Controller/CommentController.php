a698954c40e2b5b90cf1a47652faceaff4310a38
Taras <refost220@gmail.com>
2021-08-12 23:38:57 +0300

    template works

--- web/modules/custom/guestBook/src/Controller/CommentController.php
+++ web/modules/custom/guestBook/src/Controller/CommentController.php
@@ -6,11 +6,58 @@ use Drupal\Core\Controller\ControllerBase;
 
 class CommentController extends ControllerBase{
 
+  /**
+   * Create table with cats.
+   */
+  public function table():array {
+    $query = \Drupal::database()->select('guest_book', 'comment');
+    $query->fields('comment', ['name', 'email', 'phone', 'comment', 'date']);
+    $results = $query->execute()->fetchAll();
+    $commment = [];
+    foreach ($results as $data) {
+//      $fid = $data->image;
+//      $file = File::load($fid);
+//      $path = $file->getFileUri();
+//
+//      $image_render = [
+//        '#theme' => 'image_style',
+//        '#style_name' => 'thumbnail',
+//        '#uri' => $path,
+//      ];
+
+      $commment[] = [
+        'name' => $data->name,
+        'email' => $data->email,
+        'phone' => $data->phone,
+        'comment' => $data->comment,
+//        'avatar' => ['data' => $image_render],
+//        'avatar' => ['data' => $image_render],
+        'date' => date('Y-m-d', $data->date),
+      ];
+    }
+
+    if ($commment != NULL) {
+      krsort($commment);
+    }
+
+    var_dump($commment);
+
+    return $commment;
+  }
+
 
   public function content():array{
     $form = \Drupal::formBuilder()->getForm('Drupal\guestBook\Form\AddCommentForm');
 
-    return $form;
+    $data = $this->table();
+
+    return [
+      '#theme' => 'guestBook_template',
+      '#form' => $form,
+      '#data' => $data,
+    ];
   }
+
+
 
 }
